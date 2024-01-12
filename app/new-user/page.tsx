67c623cbe011c774f6330fe9395ccc8114af8480
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-01-12 20:31:46 +0100

    Create a new user

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,31 @@
-const NewUser = () => {
-  return <div>Hello New User</div>
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('journal')
+}
+
+const NewUser = async () => {
+  await createNewUser()
+  return <div>...loading</div>
 }
 
 export default NewUser
