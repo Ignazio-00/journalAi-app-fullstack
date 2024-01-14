04111368fd6e0d93ee0e4aa7ef95acc56dab3bfc
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-01-14 21:39:24 +0100

    Display Entry details

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,4 +1,27 @@
-const EntryPage = ({ params }) => {
-  return <div>{params.id}</div>
+import Editor from '@/components/Editor'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntry = async (id) => {
+  const user = await getUserByClerkId()
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return entry
+}
+
+const EntryPage = async ({ params }) => {
+  const entry = await getEntry(params.id)
+  return (
+    <div>
+      <Editor entry={entry} />
+    </div>
+  )
 }
 export default EntryPage
