cecb64891331fe7eadb724ed99047cb209f82465
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-01-14 21:14:44 +0100

    EntryCard Component UI

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,5 +1,12 @@
 const EntryCard = ({ entry }) => {
-  return <div>{entry.id}</div>
+  const date = new Date(entry.CreatedAt).toDateString()
+  return (
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5">{date}</div>
+      <div className="px-4 py-5">SUMMARY</div>
+      <div className="px-4 py-4">MOOD</div>
+    </div>
+  )
 }
 
 export default EntryCard
