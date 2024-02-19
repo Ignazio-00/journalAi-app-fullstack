7a59ce0ce58ad8885b39ecea35bc52f772d6f708
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-02-19 10:38:07 +0100

    Fix EntryCard dates

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,10 +1,10 @@
 const EntryCard = ({ entry }) => {
-  const date = new Date(entry.CreatedAt).toDateString()
+  const date = new Date(entry.createdAt).toDateString()
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
       <div className="px-4 py-4">{date}</div>
-      <div className="px-4 py-4">SUMMARY</div>
-      <div className="px-4 py-4">MOOD</div>
+      <div className="px-4 py-4">Summary</div>
+      <div className="px-4 py-4">Mood</div>
     </div>
   )
 }
